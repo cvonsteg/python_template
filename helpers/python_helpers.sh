2b286a7eec12d0ae9d240145646d5c6c9ffe1691
cvonsteg <cvonstegmann@gmail.com>
2019-10-11 13:11:24 +0100

    initial config and shell scripts

---
+++ helpers/python_helpers.sh
@@ -0,0 +1,41 @@
+#!bin/bash
+
+alias pyt="py.test -W ignore tests/"
+
+function init_venv() {
+  venv_dir=$1
+  python3.7 -m venv "$venv_dir"
+  venv "$venv_dir"
+  pip install wheel
+  if [ -f requirements.txt ]; then
+    pip install -r requirements.txt
+  fi
+  if [ -f requirements_dev.txt ]; then
+    pip install -r requirements_dev.txt
+  fi
+}
+
+function mkvenv() {
+  venv_dir=${1:-.venv}
+  if [ ! -d "$venv_dir" ]; then
+    init_venv "$venv_dir" >&2
+  fi
+  echo "$venv_dir"
+}
+
+function venv() {
+  venv_dir=$(mkvenv "$1")
+  . $venv_dir/bin/activate
+}
+
+function pykernel() {
+    if [ ! -a "$VIRTUAL_ENV" ]
+    then 
+        venv .venv
+    fi
+    default_name=${PWD##*/}
+    pip install ipykernel
+    ipython kernel install --user --name=${1:-$default_name}
+}
+
+
---
+++ run_setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source helpers/python_helpers.sh
+venv 
+pre-commit install
